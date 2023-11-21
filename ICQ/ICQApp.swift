d88cae7446cb6044f07e7cad8d409d01635884e8
Vladimir Fibe <motiw@icloud.com>
2023-11-21 13:48:20 +0600

    Initial Commit

---
+++ ICQ/ICQApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICQApp.swift
+//  ICQ
+//
+//  Created by Vladimir Fibe on 21.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ICQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
